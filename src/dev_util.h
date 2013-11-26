5ff56773854c7c62de51c12c239658e873b0ea03
David C Black <dcblack@hldwizard.com>
2013-11-26 10:33:34 -0600

    moved cmd_state_name() from command.cpp

--- src/dev_util.h
+++ src/dev_util.h
@@ -2,8 +2,11 @@
 #define DEV_UTIL_H
 
 #include "dev_hls.h"
+#include <string>
 
 extern const char* const reg_name[];
 extern const char* const operation_name[];
+extern const char* const operation_name[];
+extern std::string cmd_state_name(Data_t status);
 
 #endif /*DEV_UTIL_H*/
